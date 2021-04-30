3b927d5e06e4e549d3312385c55595f62e0bdf0f
martin lowe <martinlowe673@gmail.com>
2021-04-30 10:59:58 +0100

    new information

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const todos = ["get on the bus", "take bin out"];
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      {todos.map((todo) => (
+        <p>{todo}</p>
+      ))}
     </div>
   );
 }
