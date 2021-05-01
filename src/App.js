ddcd392d881ba55710c0203f2ac8ebe84eee110c
martin lowe <martinlowe673@gmail.com>
2021-05-01 11:24:51 +0100

    added on change

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Todo from "./Components/Todo";
 import { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const todos = ["get on the bus", "take bin out"];
+//initial value and state
+const initialState = ["get on the bus", "take bin out"];
 
+function App() {
+  const [todos, setTodos] = useState(initialState);
+  // user input values
+  const [inputValue, setInputValue] = useState("");
+
+  // everytime the user puts information it will change because of on change, on change is set to empty because we dont know what user will put in
   return (
     <div>
-      <h1> Todo List§</h1>
+      <h1> Todo List</h1>
       <div>
-        <input></input>
-        <button>Add todo</button>
+        <input
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        ></input>
+        <button onClick={(e) =>}>Add todo</button>
       </div>
       {todos.map((todo) => (
         <Todo todo={todo} />
