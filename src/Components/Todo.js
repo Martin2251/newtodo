4f792d96a87833b4c9ca79f8d659d08161becdc0
martin lowe <martinlowe673@gmail.com>
2021-05-01 11:41:30 +0100

    added remove and css

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Todo(props) {
-  return <div className="todo">{props.todo}</div>;
+  return (
+    <div className="todo">
+      <p>{props.todo}</p>
+      <button>X</button>
+    </div>
+  );
 }
